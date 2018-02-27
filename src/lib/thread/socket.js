7a8f3e62a3359ca03232a3f6b76debba7a9742c5
Alex Kanunnikov <Aleksandr_Kanunnikov@epam.com>
2018-02-27 19:06:46 +0300

    webworker runner

--- src/lib/thread/socket.js
+++ src/lib/thread/socket.js
@@ -1,5 +1,21 @@
+var Worker = require('webworker-threads').Worker;
+// var w = new Worker('worker.js'); // Standard API
+
+// You may also pass in a function:
+var worker = new Worker(function () {
+	postMessage("I'm working before postMessage('ali').");
+	this.onmessage = function (event) {
+		postMessage('Hi ' + event.data);
+		self.close();
+	};
+});
+worker.onmessage = function (event) {
+	console.log("Worker said : " + event.data);
+};
+worker.postMessage('ali');
+
 const requireJS = function(scriptName) {
-	require(`./${scriptName}`);
+	importScripts(`./${scriptName}`);
 };
  
 requireJS('globals.js');
